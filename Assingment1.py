88d9dc5ca4450c89702efd3d64ec5ff51df74d4f
wahid1099 <wahidahmed890@gmail.com>
2023-08-27 23:30:09 +0600

    adding all

---
+++ Assingment1.py
@@ -0,0 +1,76 @@
+# 1. Print your name.
+print("My name is MD WAHID")
+
+# 2. Input your batch number using input() function.
+batch_number = input("Please enter your batch number: ")
+print("You batch number is:", batch_number)
+
+# 3. Create 5 different valid Variables and print.
+
+
+name = "Md Wahid"
+Age = 42
+CGPA = 3.80
+Passed = True
+Semester_completed = [1, 2, 3]
+
+# Printing the variables
+print("Name is:", name)
+print("Age:", Age)
+print("CGPA:", CGPA)
+print("Passed:", Passed)
+print("Semester_completed:", Semester_completed)
+
+
+# 4. Create Pascal Case Variable.
+BestDjangoCourseInBd = "Study Mart Django Course"
+print(BestDjangoCourseInBd)
+
+# 5. Assign a single value to Multiple variables.
+x = y = z = 10
+
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
+
+# 6. Input float value using input().
+user_input = float(input("Enter a floating-point number: "))
+print("You entered:", user_input)
+
+
+# 7. a = ‘Welcome to Django for web and Ai’ convert into upper case.
+a = "Welcome to Django for web and Ai"
+print(a.upper())
+
+# 8. b = ‘ Python’remove whitespace.
+b = "       Python"
+
+print(b.strip())
+
+
+# 9. c = 60, d= 22 ‘Conversion int to float’.
+c = 60
+d = 22
+
+
+print("Float value of c:", float(c))
+print("Float value of d:", float(d))
+
+
+# 10. Create a dictionary & print your info.
+my_info = {
+    "name": "MD WAHID",
+    "age": 23,
+    "occupation": "Software Engineer",
+    "location": "Dhaka",
+    "email": "wahid@gmail.com",
+}
+
+# Print the dictionary
+print("My Information:")
+print("Name:", my_info["name"])
+print("Age:", my_info["age"])
+print("Occupation:", my_info["occupation"])
+print("Location:", my_info["location"])
+print("Email:", my_info["email"])
